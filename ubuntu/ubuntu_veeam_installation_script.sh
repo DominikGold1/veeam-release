95112632d5bd2b23e32732076a84453136d0dcd4
Dominik Goldschmidt (dgoldsch) <dgoldsch@cisco.com>
2020-03-16 13:10:43 +0100

    script changed

--- ubuntu/ubuntu_veeam_installation_script.sh
+++ ubuntu/ubuntu_veeam_installation_script.sh
@@ -1,9 +1,9 @@
 chmod +x ./ubuntu_veeam_installation_script.sh
 sudo apt update
-sudo apt install -y git
+sudo apt -y install git
 git clone https://github.com/DominikGold1/veeam-release
 cd veeam-release/ubuntu/
-sudo apt-get install lvm2
+sudo apt-get -y install lvm2
 sudo dpkg -i ./veeamsnap* && sudo apt-get update
 sudo dpkg -i ./veeam_* && sudo apt-get update
 sudo apt-get -y install veeam
