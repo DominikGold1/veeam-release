35429193b09e55449a8497554a0c1de352776475
DominikGold1 <58692783+DominikGold1@users.noreply.github.com>
2020-03-16 12:34:47 +0100

    Add files via upload

--- ubuntu/ubuntu_veeam_installation_script.sh
+++ ubuntu/ubuntu_veeam_installation_script.sh
@@ -1,9 +1,9 @@
-
 chmod +x ./ubuntu_veeam_installation_script.sh
 sudo apt update
 sudo apt install git
 git clone https://github.com/DominikGold1/veeam-release
 cd veeam-release/ubuntu/
 sudo dpkg -i ./veeam_* && sudo apt-get update
+>>>>>>> 64e3f400f2a9db8f99e1d1bec6d0cbc9f84f191f
 sudo dpkg -i ./veeamsnap* && sudo apt-get update
 sudo apt-get -y install veeam
