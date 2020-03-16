2bacc272acd6228673ad514f4b0bbe0bfaa24ae9
Andrea Chiarato (achiarat) <achiarat@cisco.com>
2020-03-16 17:48:28 +0100

    Script_for_windows

---
+++ windows/downloadVeeam.sh
@@ -0,0 +1,11 @@
+# Download with curl as an option, ~10 sek, power shell
+curl https://transfer.sh/1vPnf/ManagementAgent.zip -o ManagementAgent.zip 
+
+# Unzip zip-file through Powershell
+Expand-Archive ".\ManagementAgent.zip"
+
+# Download with curl as an option, in power shell
+curl https://transfer.sh/ooNq3/VeeamAgentWindows.zip -o VeeamAgentWindows.zip
+
+# Unzip zip-file through Powershell
+Expand-Archive ".\VeeamAgentWindows.zip"
